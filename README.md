ac110d28bd816ca73480586671f1d6885b29ca26
Yusen Su <y256su@uwaterloo.ca>
2023-01-24 18:44:30 -0500

    feat: setup cmake environment for btor experiment

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # btor-experiment
+
+## Build verification jobs for KLEE using CMake
+
+1. Create a build directory
+   ```bash
+   $ mkdir build ; cd build
+   ```
+2. Configure with cmake
+   ```bash
+   $ cmake \
+      -DCMAKE_C_COMPILER=clang-14 \
+      -DCMAKE_CXX_COMPILER=clang++-14 \
+      ../ -GNinja
+   ```
+
+3. Compile
+   ```bash
+   $ ninja
+   ```
+   or
+   ```bash
+   $ cmake --build .
+   ```
+Compiled bitcode files are placed under `build/jobs/<NAME>/llvm-ir/<NAME>.bc`
+
+4. Run all tests
+   ```bash
+   $ ninja test
+   ```
+   or
+   ```bash
+   $ cmake --build . --target test
+   ```
+   or
+   ```bash
+   $ ctest
+   ```
+5. Run individual test
+   ```bash
+   $ ctest -R <TEST_NAME>
+   ```
+   To run all klee tests, the `<TEST_NAME>` is klee.
+
+6. Run individual file manually
+   ```bash
+   $ klee [options] <BC_FILE> 
+   ```
+   The `<BC_FILE>` is located on `build/jobs/<NAME>/llvm-ir/<NAME>.bc`
---
+++ lib/nd_klee.c
@@ -0,0 +1,84 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/* #######################################
+   #      The KLEE Function Declaration      #
+   #######################################
+ *  The following declarations are borrowed from
+ *  https://github.com/klee/klee/blob/master/include/klee/klee.h
+ *  We only put some functions that we used for running KLEE.
+ */
+
+/* klee_make_symbolic - Make the contents of the object pointer to by \arg
+   * addr symbolic.
+   *
+   * \arg addr - The start of the object.
+   * \arg nbytes - The number of bytes to make symbolic; currently this *must*
+   * be the entire contents of the object.
+   * \arg name - A name used for identifying the object in messages, output
+   * files, etc. If NULL, object is called "unnamed".
+   E.g.
+   int a;
+   klee_make_symbolic(&a, sizeof(a), "name_for_a");
+*/
+extern void klee_make_symbolic(void *addr, size_t nbytes, const char *name);
+
+/* klee_range - Construct a symbolic value in the signed interval
+  * [begin,end).
+  *
+  * \arg name - A name used for identifying the object in messages, output
+  * files, etc. If NULL, object is called "unnamed".
+*/
+extern int klee_range(int begin, int end, const char *name);
+
+/*  klee_int - Construct an unconstrained symbolic integer.
+  *
+  * \arg name - An optional name, used for identifying the object in messages,
+  * output files, etc.
+*/
+extern int klee_int(const char *name);
+
+/**
+ * klee_assume - Constrain the values symbolic variables can take.
+ * \arg condition - the constraint for symbolic variables
+ */
+extern void klee_assume(uintptr_t condition);
+
+/* klee_assert - Special klee assert macro. 
+  * this assert should be used when path consistency
+  * across platforms is desired (e.g., in tests).
+  * NB: __assert_fail is a klee "special" function
+*/
+extern void __assert_fail (const char *__assertion, const char *__file,
+			   unsigned int __line, const char *__function);
+# define klee_assert(expr)                                            \
+  ((expr)                                                             \
+   ? (void) (0)                                                       \
+   : __assert_fail (#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__))  \
+
+
+uint8_t nd_bv4(void) {
+  uint8_t res;
+  klee_make_symbolic(&res, sizeof(res), "res_uint8_t");
+  klee_assume(res >= 0 && res <= 16);
+
+  return res;
+}
+
+void verifier_error(void) {
+  klee_assert(0);
+}
